66c5d85d84b2251f822cfc149352170bf596302c
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-11-18 08:52:54 -0400

    ES7

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+
+// Clase #6 ¿Qué se implementó en ES7?
+
+// Object.includes();
+
+let numbers = [1,2,3,7,5,6,9];
+
+if (numbers.includes(7)) {
+    console.log("Si se encuentra el valor 7");
+} else {
+    console.log("No se encuentra el valor 7");
+}
+
+// Elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
